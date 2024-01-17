8c848fa038794b191a2177ac1a7068ec1fcc1462
vaibhav <vaibhavmasetty06@gmail.com>
2024-01-17 15:11:16 -0500

    first commit

---
+++ deliveryboy/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.deliveryboy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+public class AppConstants {
+
+    public static final String LOCATION_TOPIC_NAME="location-update-topic";
+}
